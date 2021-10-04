44893a2fadf5ea7b6a84c6dfe7c06f089a2a13b3
sayali-2000 <ghadge695@gmail.com>
2021-10-04 22:21:20 +0530

    first commit

---
+++ Graphs.py
@@ -0,0 +1,19 @@
+class Graph:
+    def __init__(self,edges):
+        self.edges = edges
+        self.graph = {}
+        for start,end in self.edges:
+            if start in self.graph:
+                self.graph[start].append(end)
+            else:
+                self.graph[start] = [end]
+        print(self.graph)
+routes = [
+    ("Mumbai","Pune"),
+    ("Nashik", "Ahmednagar"),
+    ("Ahmednagar", "Pune"),
+    ("Pune","Ratnagiri"),
+    ("Mumbai","Ratnagiri"),
+    ("Pune", "Nagpur")
+]
+r1 = Graph(routes)
